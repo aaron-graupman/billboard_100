89333e014173d632073734806cc17a3365a46f3c
Aaron Graupman <aaron.emmi3@gmail.com>
2017-06-02 10:17:01 -0600

    added shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :artists
+
   resources :billboards
   root 'billboards#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601201443) do
+ActiveRecord::Schema.define(version: 20170602152854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "sales", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "billboards", force: :cascade do |t|
     t.string "title", null: false
